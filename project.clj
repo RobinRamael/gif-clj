ec117f5ba22dd284479bc4df50e57418a8b88543
Robin Ramael <robin.ramael@gmail.com>
2011-08-09 16:37:30 +0200

    added project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject gif-clj "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "a small gif encoder"
   :dependencies [[org.clojure/clojure "1.2.1"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
   :java-source-path "src/java")
