3194b5ddbc0c4bae24e7df075f64cdaebee386d5
zachcp <zcharlop@rockefeller.du>
2014-10-27 23:19:55 -0400

    Version Bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gif-clj "1.0.0-SNAPSHOT"
+(defproject gif-clj "1.0.1"
   :description "a small gif encoder"
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
