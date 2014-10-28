16618f7b3db7835fae46cfce23691ad7b3e94776
zachcp <zcharlop@rockefeller.du>
2014-10-27 23:19:30 -0400

    simple documentation
    documentaiton

--- src/gif_clj/core.clj
+++ src/gif_clj/core.clj
@@ -1,8 +1,11 @@
 (ns gif-clj.core
   (:import AnimatedGifEncoder))
 
-(defn write-gif [filename imgs {:keys [delay loops]}
-                                :or {delay 1000 loops 0}]
+(defn write-gif 
+  "create an animated gif from a series of images.
+  (write-gif "output.gif" [image1 image2 image3])"
+  [filename imgs {:keys [delay loops]}
+                  :or {delay 1000 loops 0}]
   (let [encoder
 	(doto (AnimatedGifEncoder.)
 	  (.start filename)
