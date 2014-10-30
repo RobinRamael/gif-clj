c1f55473768a6ded653626c2af493b4122866a74
zach powers <zcharlop@rockefeller.edu>
2014-10-30 10:45:56 -0400

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gif-clj "1.0.2"
+(defproject gif-clj "1.0.3"
   :description "a small gif encoder"
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
