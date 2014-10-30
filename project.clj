81c9fb17d5e9c18dffcb91db8e9c222d876a8a49
zach powers <zcharlop@rockefeller.edu>
2014-10-30 11:07:09 -0400

    add license and url for clojars

--- project.clj
+++ project.clj
@@ -2,4 +2,6 @@
   :description "a small gif encoder"
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
-  :java-source-paths ["src/java"])
+  :java-source-paths ["src/java"]
+  :license "Eclipse Public License"
+  :url "https://github.com/zachcp/gif-clj")
